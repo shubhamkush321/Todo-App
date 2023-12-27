156c876ca3b2256902e1dd64d1b1bcec61b6a193
shubham <142429585+shubhamkush321@users.noreply.github.com>
2023-12-27 22:27:10 +0530

    Todo App

---
+++ src/TodoList.jsx
@@ -0,0 +1,105 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import './App.css';
+
+
+export default function TodoList() {
+  const [todos, setTodos] = useState([
+    { task: "Sample-Task", id: uuidv4(), isDone: false },
+  ]);
+  const [newTodo, setNewTodo] = useState("");
+  const [editingId, setEditingId] = useState(null);
+
+  const addNewTask = () => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { task: newTodo, id: uuidv4(), isDone: false },
+    ]);
+    setNewTodo("");
+  };
+
+  const updateTodoValue = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  const upperCaseAll = () => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => ({ ...todo, task: todo.task.toUpperCase() }))
+    );
+  };
+
+  const markAsDone = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => ({
+        ...todo,
+        isDone: todo.id === id ? true : todo.isDone,
+      }))
+    );
+  };
+
+  const startEditing = (id) => {
+    setEditingId(id);
+  };
+
+  const saveEdit = (id, updatedTask) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, task: updatedTask };
+        }
+        return todo;
+      })
+    );
+    setEditingId(null);
+  };
+
+  return (
+    <div>
+      <h2>Todo App</h2>
+      <input
+        placeholder="Add a task"
+        value={newTodo}
+        onChange={updateTodoValue}
+      />
+      <button onClick={addNewTask}>Add Task</button>
+      <br /> <br />
+
+      <h3>Task</h3>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {editingId === todo.id ? (
+              <>
+                <span style={todo.isDone ? { textDecoration: "line-through" } : {}}>
+                  {todo.task}
+                </span>
+                <input
+                  type="text"
+                  value={todo.task}
+                  onChange={(e) => saveEdit(todo.id, e.target.value)}
+                />
+                <button onClick={() => saveEdit(todo.id, todo.task)}>Save</button>
+              </>
+            ) : (
+              <>
+                <span style={todo.isDone ? { textDecoration: "line-through" } : {}}>
+                  {todo.task}
+                </span>
+                <button onClick={() => deleteTodo(todo.id)}>Delete</button> <br />
+                <button onClick={() => markAsDone(todo.id)}>Done</button> <br />
+                <button onClick={() => startEditing(todo.id)}>Edit</button>
+              </>
+            )}
+          </li>
+        ))}
+      </ul>
+      <br />
+      <button onClick={upperCaseAll}>Uppercase</button>
+    </div>
+  );
+}
